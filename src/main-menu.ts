2e42aef16e3424d71609fa8f39c7b21120d9136b
Mateusz Tomczyk <tulfm@poczta.fm>
2018-08-30 22:46:00 +0200

    added game objective to menus

--- src/main-menu.ts
+++ src/main-menu.ts
@@ -12,6 +12,8 @@ export class MainMenu {
 
   controlsMenu = new Menu();
 
+  objectiveMenu = new Menu();
+
   constructor(private game: Game) {
     this.menu.addOption({
       text: 'Start new game',
@@ -44,6 +46,25 @@ export class MainMenu {
     this.controlsMenu.addStaticOption('C - toggle sneak/run');
     this.controlsMenu.addStaticOption('SHIFT - sneak/run');
     this.controlsMenu.addStaticOption('ESC - toggle pause and menu');
+
+    this.menu.addSubmenu('Game objective', this.objectiveMenu);
+
+    this.objectiveMenu.addOption({
+      text: 'Back',
+      callback: () => this.objectiveMenu.backToParent(),
+    })
+
+    this.objectiveMenu.addStaticOption('Fight your way back to home through the');
+    this.objectiveMenu.addStaticOption('Shutter Asylum.');
+
+    this.objectiveMenu.addStaticOption('The game\'s main objective is to kill');
+    this.objectiveMenu.addStaticOption('each enemy in every stage.');
+
+    this.objectiveMenu.addStaticOption('Additional objective is to speed run');
+    this.objectiveMenu.addStaticOption('each level, best times are recorded.');
+
+    this.objectiveMenu.addStaticOption('Use stealth, you won\'t survive on');
+    this.objectiveMenu.addStaticOption('higher difficulties by being Rambo.');
 
     this.updateLevelsList();
   }
